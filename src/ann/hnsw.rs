30518c8d234ad78ad99a50ef61531057725f632d
szuwgh <895340293@qq.com>
2022-09-30 18:56:38 +0800

    coding hnsw

--- src/ann/hnsw.rs
+++ src/ann/hnsw.rs
@@ -1,1 +1,16 @@
 pub struct HNSW {}
+
+struct Neighbor {
+    id: u32,
+    d: f32, //distance
+}
+
+impl HNSW {
+    fn insert(q: Vec<f32>, id: u32) {}
+
+    fn search_at_layer(q: Vec<f32>, level: u32) {
+        let candidates: Vec<Neighbor> = Vec::new();
+    }
+
+    fn search() {}
+}
