082b6e4fd8ae1b91eb60b7747d5979fbd799cdd2
szuwgh <895340293@qq.com>
2022-10-08 19:55:10 +0800

    coding hnsw

--- src/ann/hnsw.rs
+++ src/ann/hnsw.rs
@@ -1,4 +1,6 @@
-pub struct HNSW {}
+pub struct HNSW {
+    enter_point: u32,
+}
 
 struct Neighbor {
     id: u32,
@@ -6,11 +8,27 @@ struct Neighbor {
 }
 
 impl HNSW {
-    fn insert(q: Vec<f32>, id: u32) {}
+    fn get_random_level() {}
+
+    fn insert(&mut self, q: Vec<f32>, id: u32) {
+        //let cur_level =
+    }
 
     fn search_at_layer(q: Vec<f32>, level: u32) {
         let candidates: Vec<Neighbor> = Vec::new();
     }
 
     fn search() {}
+}
+
+#[cfg(test)]
+mod tests {
+    use rand::{thread_rng, Rng};
+
+    #[test]
+    fn test_hnsw() {
+        let mut rng = rand::thread_rng();
+        let x: f64 = rng.gen();
+        println!("x = {x}");
+    }
 }
